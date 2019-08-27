af58b4118136f055ead606f462d3a2ba07f876ed
Alexsandro <alexsandro.sfo@gmail.com>
2019-08-27 08:20:55 -0300

    1 ok

--- aula 03/calculadora.cpp
+++ aula 03/calculadora.cpp
@@ -4,46 +4,44 @@ using namespace std;
 int main(){
 
     int opcao = -1;
-    bool cont = false;
-
-    while (cont == false){
+    bool condicao;
     float n1, n2;
-    cout << "Informe o primeiro numero: ";
-    cin >> n1;
-    cout << "Informe o segundo numero: ";
-    cin >> n2;
+    
+    while (condicao) {
 
     cout << "Informe a operação desejada: " << endl << "1 - soma" << endl << "2 - subtração"<<endl<< "3 - divisão" <<endl<<"4 - multiplicação" << endl <<  endl <<"0 - encerrar" << endl;  ;
     cin >> opcao;
 
+    if (opcao != 0){
+        cout << "Informe o primeiro numero: ";
+        cin >> n1;
+        cout << "Informe o segundo numero: ";
+        cin >> n2;
+    }
+   
     switch (opcao){
         case 1:
-        cout << "Resultado: " << n1 + n2 << endl;
+        cout << endl << "Resultado: " << n1 + n2 << endl << endl;
         break;
 
         case 2:
-        cout << "Resultado: " << n1 - n2 << endl;
+        cout << endl << "Resultado: " << n1 - n2 << endl << endl;
         break;
 
         case 3:
-        cout << "Resultado: " << n1 / n2 << endl;
+        cout << endl << "Resultado: " << n1 / n2 << endl << endl;
         break;
 
         case 4:
-        cout << "Resultado: " << n1 * n2 << endl;
+        cout << endl << "Resultado: " << n1 * n2 << endl << endl;
         break;
 
         case 0:
-        cont == true;
-        cout << "Encerrado.";
-        exit;
-        break;
-
+        condicao = false;
+        cout << endl << "Encerrado.";
     default:
-        break;
+        continue;
     }
-
- 
     }
 
     return 0;
